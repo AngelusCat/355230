a5ac84d5d5a1b87aef51fe86ea50dde661857c10
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-10-29 09:46:45 +0300

    ...

---
+++ app/IdentityMaps/TicketMap.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\IdentityMaps;
+
+class TicketMap
+{
+
+}
---
+++ app/Services/EventMapper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\Event;
+
+class EventMapper
+{
+    public function getEvent(int $eventId): Event
+    {
+        
+    }
+}
